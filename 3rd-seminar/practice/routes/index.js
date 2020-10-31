866b490929951f530d1123930d9e5f2f4df2f307
jaeukyang <didwodnr123@gmail.com>
2020-10-31 14:13:49 +0900

    ADD: express practice

--- 3rd-seminar/practice/routes/index.js
+++ 3rd-seminar/practice/routes/index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+router.use('/users', require('./users'))
+router.use('/ranking', require('./ranking'))
+router.use('/society', require('./society'))
+router.use('/members', require('./members'))
+
+router.use('/', (req, res)=>{
+  res.status(200).send('서버파트 짱짱맨');
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
